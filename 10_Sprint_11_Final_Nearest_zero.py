37dc21696880d279e7e0c2c56d1ae131d3fa196c
Alexsandr <kireev20000@mail.ru>
2023-03-30 15:41:34 +0700

    Тимофей ищет место, чтобы построить себе дом. Улица, на которой он хочет жить, имеет длину n, то есть состоит из n одинаковых идущих подряд участков. Каждый участок либо пустой, либо на нём уже построен дом.
    Общительный Тимофей не хочет жить далеко от других людей на этой улице. Поэтому ему важно для каждого участка знать расстояние до ближайшего пустого участка. Если участок пустой, эта величина будет равна нулю — расстояние до самого себя.
    
    Помогите Тимофею посчитать искомые расстояния. Для этого у вас есть карта улицы. Дома в городе Тимофея нумеровались в том порядке, в котором строились, поэтому их номера на карте никак не упорядочены. Пустые участки обозначены нулями.
    
    Формат ввода
    В первой строке дана длина улицы —– n (1 ≤ n ≤ 106). В следующей строке записаны n целых неотрицательных чисел — номера домов и обозначения пустых участков на карте (нули). Гарантируется, что в последовательности есть хотя бы один ноль. Номера домов (положительные числа) уникальны и не превосходят 109.
    
    Формат вывода
    Для каждого из участков выведите расстояние до ближайшего нуля. Числа выводите в одну строку, разделяя их пробелами.

--- 10_Sprint_11_Final_Nearest_zero.py
+++ 10_Sprint_11_Final_Nearest_zero.py
@@ -1,22 +1,23 @@
-# ID успешной посылки = 84809972
-# URL успешного отчета = https://contest.yandex.ru/contest/23390/run-report/84809972/
-
-street_length = int(input())
-house_numbers = input().split()
+# ID успешной посылки = 84836997
+# URL успешного отчета = https://contest.yandex.ru/contest/23390/run-report/84836997/
 
 def calculate_distance(street_length, house_numbers):
     distance = street_length
-    result_list = []
+    result_list = [0] * street_length
+    list_index = 0
     for n in house_numbers:
         if n == '0':
             distance = 0
         else:
             distance += 1
-        result_list.append(distance)
+        result_list[list_index] = distance
+        list_index += 1
     return result_list
 
 
-to_left = calculate_distance(street_length, house_numbers)
-to_right = reversed(calculate_distance(street_length, reversed(house_numbers)))
-
-print(*map(min, zip(to_left, to_right)))
+if __name__ == '__main__':
+    street_length = int(input())
+    house_numbers = input().split()
+    to_left = calculate_distance(street_length, house_numbers)
+    to_right = reversed(calculate_distance(street_length, reversed(house_numbers)))
+    print(*map(min, zip(to_left, to_right)))
