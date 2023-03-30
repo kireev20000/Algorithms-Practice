6d1677ef19d0951dcc34fd36791dbe87b1846902
Alexsandr <kireev20000@mail.ru>
2023-03-30 19:35:18 +0700

    Игра «Тренажёр для скоростной печати» представляет собой поле из клавиш 4x4. В нём на каждом раунде появляется конфигурация цифр и точек. На клавише написана либо точка, либо цифра от 1 до 9.
    В момент времени t игрок должен одновременно нажать на все клавиши, на которых написана цифра t. Гоша и Тимофей могут нажать в один момент времени на k клавиш каждый. Если в момент времени t нажаты все нужные клавиши, то игроки получают 1 балл.
    
    Найдите число баллов, которое смогут заработать Гоша и Тимофей, если будут нажимать на клавиши вдвоём.
    
    Формат ввода
    В первой строке дано целое число k (1 ≤ k ≤ 5).
    
    В четырёх следующих строках задан вид тренажёра -— по 4 символа в каждой строке. Каждый символ – либо точка, либо цифра от 1 до 9. Символы одной строки идут подряд и не разделены пробелами.
    
    Формат вывода
    Выведите единственное число -— максимальное количество баллов, которое смогут набрать Гоша и Тимофей.

--- 11_Sprint_11_Final_Sleight_of_Hand.py
+++ 11_Sprint_11_Final_Sleight_of_Hand.py
@@ -1,25 +1,18 @@
-# ID успешной посылки = 84815553
-# URL успешного отчета = https://contest.yandex.ru/contest/23390/run-report/84815553/
+# ID успешной посылки = 84851513
+# URL успешного отчета = https://contest.yandex.ru/contest/23390/run-report/84851513/
 
-def count_points():
-    k = int(input())
-    game_field = ''
-    game_field = ''.join(map(str, [game_field + input() for x in range(4)]))
-    #k = 3
-    #game_field = '1111999911119911'
-    #game_field = '12312..22..22..2'
-    appearance_of_digits = []
-
-    for i in range(1, 10):
-        digits_count = game_field.count(str(i))
-        appearance_of_digits.append(digits_count)
-
+def count_points(k, game_field):
     points = 0
-    for i, digit_quantity in enumerate(appearance_of_digits):
-        if digit_quantity == 0:
+    for i in range(1, 10):
+        digits_count = int(game_field.count(str(i)))
+        if digits_count == 0:
             continue
-        elif int(digit_quantity) <= (k*2):
+        elif int(digits_count) <= (k*2):
             points += 1
     return points
 
-print(count_points())
+
+if __name__ == '__main__':
+    k = int(input())
+    game_field = ''.join([input() for _ in range(4)])
+    print(count_points(k, game_field))
