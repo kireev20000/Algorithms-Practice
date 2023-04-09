85e11fa4d54356be3a157543b81fa0c298b12af4
Alexsandr <kireev20000@mail.ru>
2023-04-10 06:25:28 +0700

    Вот какую задачу Тимофей предложил на собеседовании одному из кандидатов. Если вы с ней ещё не сталкивались, то наверняка столкнётесь –— она довольно популярная.
    Дана скобочная последовательность. Нужно определить, правильная ли она.
    
    Будем придерживаться такого определения:
    
    пустая строка —– правильная скобочная последовательность;
    правильная скобочная последовательность, взятая в скобки одного типа, –— правильная скобочная последовательность;
    правильная скобочная последовательность с приписанной слева или справа правильной скобочной последовательностью —– тоже правильная.
    На вход подаётся последовательность из скобок трёх видов: [], (), {}.
    Напишите функцию is_correct_bracket_seq, которая принимает на вход скобочную последовательность и возвращает True, если последовательность правильная, а иначе False.
    
    Формат ввода
    На вход подаётся одна строка, содержащая скобочную последовательность. Скобки записаны подряд, без пробелов.
    
    Формат вывода
    Выведите «True» или «False».

--- Sprint_12_08_Scobochnay_Sequence.py
+++ Sprint_12_08_Scobochnay_Sequence.py
@@ -1,6 +1,8 @@
+# классная статья про это - https://medium.com/@vitkarpov/%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F-%D1%81%D0%BA%D0%BE%D0%B1%D0%BE%D1%87%D0%BD%D0%B0%D1%8F-%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C-98699eebd5ac
 class Stack:
     def __init__(self):
-            self.items = []
+        self.items = []
+
     def if_empty(self):
         return self.items == []
 
@@ -13,15 +15,15 @@ class Stack:
     def peek(self):
         return self.items[len(self.items)-1]
 
+
 line = list(input())
 s = Stack()
-d = {'(':')','{':'}','[':']'}
-
+dict_examples = {'(': ')', '{': '}', '[': ']'}
 
 for x in line:
-    if x in d.keys():
+    if x in dict_examples.keys():
         s.push(x)
-    elif not s.if_empty() and d[s.peek()] == x:
+    elif not s.if_empty() and dict_examples[s.peek()] == x:
         s.pop()
     else:
         s.push(x)
