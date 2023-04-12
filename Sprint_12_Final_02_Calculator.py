bab6e952f6330f46222ac1fdd41dfbac3659d8f5
Alexsandr <kireev20000@mail.ru>
2023-04-12 17:52:02 +0700

    Алгоритм для расшифровки польской нотации.

---
+++ Sprint_12_Final_02_Calculator.py
@@ -0,0 +1,38 @@
+# ID успешной посылки = 85649719
+# URL успешного отчета = https://contest.yandex.ru/contest/23759/run-report/85649719/
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, i):
+        self.items.append(i)
+
+    def pop(self):
+        if len(self.items) > 0:
+            return self.items.pop()
+        raise IndexError('ОШИБКА! Стек пуст!')
+
+
+def calculator(input_data):
+    stack = Stack()
+    for i in input_data:
+        if i not in "+, -, *, / ":
+            stack.push(int(i))
+            continue
+        n1, n2 = stack.pop(), stack.pop()
+        if i == "+":
+            stack.push(n1 + n2)
+        elif i == "-":
+            stack.push(n1 - n2)
+        elif i == "*":
+            stack.push(n1 * n2)
+        else:
+            stack.push(n1 // n2)
+
+    return stack.pop()
+
+
+if __name__ == '__main__':
+    input_data = input().split()
+    print(calculator(input_data))
