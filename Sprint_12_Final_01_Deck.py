2cfa9a0eb65e359578ee35f242349d70182f1cce
Alexsandr <kireev20000@mail.ru>
2023-04-12 16:28:12 +0700

    Алгоритм двухсвязянного списка.

--- Sprint_12_Final_01_Deck.py
+++ Sprint_12_Final_01_Deck.py
@@ -1,14 +1,14 @@
-# ID успешной посылки = 84836997
-# URL успешного отчета = https://contest.yandex.ru/contest/23390/run-report/84836997/
+# ID успешной посылки = 85645716
+# URL успешного отчета = https://contest.yandex.ru/contest/23759/run-report/85645716/
 
-SIZE_LIMIT = 'Дек переполнен'
-EMPTY = 'Элементы в Деке отсутсвуют'
+Deck_is_Empty_MSG = 'ОШИБКА! Дека пуста!'
+Deck_Overflow_MSG = 'ОШИБКА! В деке больше нет места!'
 
 
 class Deck:
-    def __init__(self, max_size):
-        self.queue = max_size * [None]
-        self.max_size = max_size
+    def __init__(self, max_n):
+        self.queue = max_n * [None]
+        self.max_n = max_n
         self.head = 0
         self.tail = 0
         self.size = 0
@@ -17,44 +17,44 @@ class Deck:
         return self.size == 0
 
     def size_limit(self):
-        return not(self.size != self.max_size)
+        return not(self.size != self.max_n)
 
     def push_back(self, value):
         if self.size_limit():
-            raise IndexError(SIZE_LIMIT)
+            raise IndexError(Deck_Overflow_MSG)
         self.queue[self.tail] = value
-        self.tail = (self.tail - 1) % self.max_size
+        self.tail = (self.tail + 1) % self.max_n
         self.size += 1
 
     def push_front(self, value):
         if self.size_limit():
-            raise IndexError(SIZE_LIMIT)
-        self.queue[self.head + 1] = value
-        self.head = (self.head + 1) % self.max_size
+            raise IndexError(Deck_Overflow_MSG)
+        self.queue[self.head - 1] = value
+        self.head = (self.head - 1) % self.max_n
         self.size += 1
 
     def pop_back(self):
         if self.is_empty():
-            raise IndexError(EMPTY)
-        x = self.queue[self.tail + 1]
-        self.queue[self.tail + 1] = None
-        self.tail = (self.tail + 1) % self.max_size
+            raise IndexError(Deck_is_Empty_MSG)
+        x = self.queue[self.tail - 1]
+        self.queue[self.tail - 1] = None
+        self.tail = (self.tail - 1) % self.max_n
         self.size -= 1
         return x
 
     def pop_front(self):
         if self.is_empty():
-            raise IndexError(EMPTY)
+            raise IndexError(Deck_is_Empty_MSG)
         x = self.queue[self.head]
         self.queue[self.head] = None
-        self.head = (self.head - 1) % self.max_size
+        self.head = (self.head + 1) % self.max_n
         self.size -= 1
         return x
 
 
-def deck(total_commands, max_size):
-    output = []
-    deck_arr = Deck(max_size)
+def deck(total_commands, max_n):
+
+    deck_arr = Deck(max_n)
     commands_list = {
         'push_front': deck_arr.push_front,
         'push_back': deck_arr.push_back,
@@ -62,32 +62,27 @@ def deck(total_commands, max_size):
         'pop_back': deck_arr.pop_back,
     }
 
-    a = ['push_front 861', 'push_front -819', 'pop_back', 'pop_back']
-
-    # for _ in range(total_commands):
-    for _ in a:
-        # commands = input()
-        command, *value = _.split()
+    # a = ['push_front 861', 'push_front -819', 'pop_back', 'pop_back']
+    result_list = []
+    for _ in range(total_commands):
+        command, *value = input().split()
         if value:
             try:
                 result = commands_list[command](int(*value))
                 if result is not None:
-                    output.append(result)
+                    result_list.append(result)
             except IndexError:
-                output.append('error')
+                result_list.append('error')
         else:
             try:
                 result = commands_list[command]()
-                output.append(result)
+                result_list.append(result)
             except IndexError:
-                output.append('error')
-    for result in output:
-        print(result)
+                result_list.append('error')
+
+    print(*result_list, sep="\n")
 
 
 if __name__ == '__main__':
-    # total_commands = int(input())
-    # deck_size = int(input())
-    total_commands = 4
-    deck_size = 4
+    total_commands, deck_size = int(input()), int(input())
     deck(total_commands, deck_size)
