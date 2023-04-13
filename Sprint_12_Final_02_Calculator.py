1a31c75674795c2132b1fd8057474dd2c3dd0bea
Alexsandr <kireev20000@mail.ru>
2023-04-13 12:15:51 +0700

    Алгоритм для расшифровки польской нотации. review 2

--- Sprint_12_Final_02_Calculator.py
+++ Sprint_12_Final_02_Calculator.py
@@ -1,35 +1,33 @@
-# ID успешной посылки = 85649719
-# URL успешного отчета = https://contest.yandex.ru/contest/23759/run-report/85649719/
+# ID успешной посылки = 85694448
+# URL успешного отчета = https://contest.yandex.ru/contest/23759/run-report/85694448/
 
 class Stack:
     def __init__(self):
-        self.items = []
+        self.__items = []
 
     def push(self, i):
-        self.items.append(i)
+        self.__items.append(i)
 
     def pop(self):
-        if len(self.items) > 0:
-            return self.items.pop()
+        if len(self.__items) > 0:
+            return self.__items.pop()
         raise IndexError('ОШИБКА! Стек пуст!')
 
 
 def calculator(input_data):
     stack = Stack()
+    operations = {
+        '+': lambda n1, n2: n1 + n2,
+        '-': lambda n1, n2: n2 - n1,
+        '*': lambda n1, n2: n1 * n2,
+        '/': lambda n1, n2: n2 // n1,
+    }
+
     for i in input_data:
-        if i not in "+, -, *, / ":
+        if not i in operations:
             stack.push(int(i))
             continue
-        n1, n2 = stack.pop(), stack.pop()
-        if i == "+":
-            stack.push(n1 + n2)
-        elif i == "-":
-            stack.push(n1 - n2)
-        elif i == "*":
-            stack.push(n1 * n2)
-        else:
-            stack.push(n1 // n2)
-
+        stack.push(operations[i](stack.pop(), stack.pop()))
     return stack.pop()
 
 
