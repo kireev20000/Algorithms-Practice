3f9004bec52753cc2d2cbdfb462116d268eeb54e
Alexsandr <kireev20000@mail.ru>
2023-04-13 16:13:22 +0700

    Дека review 2

--- Sprint_12_Final_01_Deck.py
+++ Sprint_12_Final_01_Deck.py
@@ -1,5 +1,5 @@
-# ID успешной посылки = 85645716
-# URL успешного отчета = https://contest.yandex.ru/contest/23759/run-report/85645716/
+# ID успешной посылки = 85703424
+# URL успешного отчета = https://contest.yandex.ru/contest/23759/run-report/85703424/
 
 Deck_is_Empty_MSG = 'ОШИБКА! Дека пуста!'
 Deck_Overflow_MSG = 'ОШИБКА! В деке больше нет места!'
@@ -38,14 +38,12 @@ class Deck:
             raise DeckIsOverflow(Deck_Overflow_MSG)
         self.__queue[self.__tail] = value
         self.__tail = self._index_position(self.__tail, +1)
-        # self.__tail = (self.__tail + 1) % self.__max_n
         self.__size += 1
 
     def push_front(self, value):
         if self._size_limit():
             raise DeckIsOverflow(Deck_Overflow_MSG)
         self.__queue[self.__head - 1] = value
-        # self.__head = (self.__head - 1) % self.__max_n
         self.__head = self._index_position(self.__head, -1)
         self.__size += 1
 
@@ -55,7 +53,6 @@ class Deck:
             raise DeckIsEmpty(Deck_is_Empty_MSG)
         x = self.__queue[self.__tail - 1]
         self.__queue[self.__tail - 1] = None
-        # self.__tail = (self.__tail - 1) % self.__max_n
         self.__tail = self._index_position(self.__tail, -1)
         self.__size -= 1
         return x
@@ -65,9 +62,7 @@ class Deck:
             raise DeckIsEmpty(Deck_is_Empty_MSG)
         x = self.__queue[self.__head]
         self.__queue[self.__head] = None
-        self.__head = (self.__head + 1) % self.__max_n
         self.__head = self._index_position(self.__head, +1)
-        # self.head = (self.head + 1) % self.max_n
         self.__size -= 1
         return x
 
@@ -75,41 +70,28 @@ class Deck:
 def deck(total_commands, max_n):
 
     deck_arr = Deck(max_n)
-    commands_list = {
-        'push_front': deck_arr.push_front,
-        'push_back': deck_arr.push_back,
-        'pop_front': deck_arr.pop_front,
-        'pop_back': deck_arr.pop_back,
-    }
 
     # a = ['push_front -201', 'push_back 959', 'push_back 102', 'push_front 20', 'pop_front', 'pop_back']
-    a = ['push_back -977', 'pop_back', 'pop_back', 'push_front -86', 'pop_back', 'push_back 81', 'pop_front',
-         'pop_back', 'pop_back']
-    result_list = []
+    #a = ['push_back -977', 'pop_back', 'pop_back', 'push_front -86', 'pop_back', 'push_back 81', 'pop_front', 'pop_back', 'pop_back']
+    a = ['pop_back', 'pop_back', 'pop_back', 'pop_front', 'pop_back', 'pop_back', 'push_front 284',
+         'pop_back', 'push_front 799', 'pop_front', 'pop_back', 'pop_back', 'pop_front', 'push_back 792',
+         'push_front 45', 'push_front 374', 'pop_back', 'push_back 740', 'pop_front', 'pop_back', 'push_front 607',
+         'push_front -492', 'push_back 661', 'push_front -551', 'pop_front']
+
     # for _ in range(total_commands):
     for _ in a:
-        # command, *value = input().split()
         command, *value = _.split()
-        if value:
-            try:
-                result = commands_list[command](int(*value))
-                if result is not None:
-                    result_list.append(result)
-            except (DeckIsOverflow, DeckIsEmpty):
-                result_list.append('error')
-        else:
-            try:
-                result = commands_list[command]()
-                result_list.append(result)
-            except (DeckIsOverflow, DeckIsEmpty):
-                result_list.append('error')
-
-    print(*result_list, sep="\n")
+        try:
+            result = getattr(deck_arr, command)(*value)
+            if result is not None:
+                print(result)
+        except (DeckIsOverflow, DeckIsEmpty):
+            print('error')
 
 
 if __name__ == '__main__':
     # total_commands, deck_size = int(input()), int(input())
-    total_commands, deck_size = 6, 6
+    total_commands, deck_size = 213, 34
     # total_commands, deck_size = 6, 6
     deck(total_commands, deck_size)
 
